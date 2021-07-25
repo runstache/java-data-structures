644938abc28d602a0daab0eb92cc7bd604cdd7e8
Larry Shields <larry.shields@gmail.com>
2021-07-25 16:37:19 -0400

    Implemented Single Linked List with Tests

---
+++ src/main/java/com/lswebworld/structures/lists/single/SingleLinkedList.java
@@ -0,0 +1,62 @@
+package com.lswebworld.structures.lists.single;
+
+public class SingleLinkedList {
+  private Node head;
+
+  public Node getHead() {
+    return head;
+  }
+
+  public void setHead(Node head) {
+    this.head = head;
+  }
+
+  /**
+   * Deletes a Given value from the list.
+   * @param key Key to remove
+   */
+  public boolean delete(int key) {
+    Node temp = head;
+    Node prev = null;
+
+    if (temp != null && temp.getData() == key) {
+      // Head is the one to delete
+      head = temp.getNext();
+      return true;
+    }
+
+    while (temp != null && temp.getData() != key) {
+      // Remember the Previous item and set temp to the next
+      prev = temp;
+      temp = temp.getNext();
+    }
+
+    if (temp == null) {
+      // Item was not found.
+      return false;
+    }
+
+    if (prev != null) {
+      if (temp.getNext() != null) {
+        // Deallocate the Temp item which contains the matched key.
+        prev.setNext(temp.getNext());
+      } else {
+        // If the last item in the list, set it to null
+        prev.setNext(null);
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Adds a Node to the Start of the list.
+   * @param data Date Value to add
+   */
+  public void push(int data) {
+    Node newNode = new Node(data);
+    newNode.setNext(head);
+    head = newNode;
+  }
+}
+
+
---
+++ src/test/java/com/lswebworld/structures/lists/single/SingleLinkedListTest.java
@@ -0,0 +1,95 @@
+package com.lswebworld.structures.lists.single;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class SingleLinkedListTest {
+
+  @Test
+  void testPushThreeNodes() {
+    SingleLinkedList list = new SingleLinkedList();
+
+    list.setHead(new Node(1));
+    list.push(2);
+    list.push(3);
+
+
+    List<Integer> values = new ArrayList<>();
+
+    Node head = list.getHead();
+    while (head != null) {
+      values.add(head.getData());
+      head = head.getNext();
+    }
+
+    assertThat(values).hasSize(3);
+    assertThat(values).containsExactly(3,2,1);
+  }
+
+  @Test
+  void testDeleteItem() {
+    SingleLinkedList list = new SingleLinkedList();
+
+    list.setHead(new Node(1));
+    list.push(2);
+    list.push(3);
+
+    assertThat(list.delete(2)).isTrue();
+    List<Integer> values = new ArrayList<>();
+
+    Node head = list.getHead();
+    while (head != null) {
+      values.add(head.getData());
+      head = head.getNext();
+    }
+
+    assertThat(values).hasSize(2);
+    assertThat(values).containsExactly(3,1);
+  }
+
+  @Test
+  void testDeleteHead() {
+    SingleLinkedList list = new SingleLinkedList();
+
+    list.setHead(new Node(1));
+    list.push(2);
+    list.push(3);
+
+    assertThat(list.delete(1)).isTrue();
+
+    List<Integer> values = new ArrayList<>();
+    Node head = list.getHead();
+    while (head != null) {
+      values.add(head.getData());
+      head = head.getNext();
+    }
+    assertThat(values).hasSize(2).containsExactly(3,2);
+
+  }
+
+  @Test
+  void testDeleteKeyNotPresent() {
+    SingleLinkedList list = new SingleLinkedList();
+
+    list.setHead(new Node(1));
+    list.push(2);
+    list.push(3);
+
+    assertThat(list.delete(4)).isFalse();
+
+    List<Integer> values = new ArrayList<>();
+    Node head = list.getHead();
+    while (head != null) {
+      values.add(head.getData());
+      head = head.getNext();
+    }
+    assertThat(values).hasSize(3).containsExactly(3,2,1);
+
+
+
+  }
+
+}
